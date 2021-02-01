420b19f80c038b37b5b69b14b07f60edc06d8314
Your Name <mehedihasan019898@gmail.com>
2021-02-01 23:52:35 +0600

    first push

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// falsy statement:
+// ""
+// 0
+//false
+// undefined
+// null
+// NaN
+// truthy string statement:
+// "0" , {}, []
+// " "
+
+
+
+// const name = 12;
+
+
+if(name.length > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
